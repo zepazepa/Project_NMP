48173b3b2366b2e58c3b20212a55dc99399ce325
kakkarot53 <valencio253@gmail.com>
2023-10-17 00:12:36 +0700

    added several activities

---
+++ app/src/main/java/com/ubayaprojectnmp/cerbung/ActivitySignUp.kt
@@ -0,0 +1,11 @@
+package com.ubayaprojectnmp.cerbung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
